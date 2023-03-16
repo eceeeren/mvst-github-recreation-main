e9795cfe7045e135efab087e6fc46092c720e112
Aybike Ece Eren <aybikeceren@gmail.com>
2023-03-17 01:24:29 +0300

    Header is enhanced

--- src/components/CustomizedHeader.jsx
+++ src/components/CustomizedHeader.jsx
@@ -1,4 +1,9 @@
-import { Header } from '@primer/react';
+import { Header, StyledOcticon, TextInput, Avatar } from '@primer/react';
+import { 
+  MarkGithubIcon, 
+  BellIcon, 
+  PlusIcon, 
+  TriangleDownIcon } from '@primer/octicons-react';
 
 
 export default function CustomizedHeader() {
@@ -6,11 +11,36 @@ export default function CustomizedHeader() {
     <Header sx={{mb: 3}}>
   <Header.Item>
     <Header.Link href="#" fontSize={2}>
-      <span>GitHub</span>
+    <StyledOcticon icon={MarkGithubIcon} size={32} sx={{mr: 2}} />
     </Header.Link>
   </Header.Item>
-  <Header.Item full>Menu</Header.Item>
+  <Header.Item>
+  <TextInput contrast aria-label="Repository" name="repository" placeholder="Search or jump to..." />
+  </Header.Item>
+  <Header.Item>
+    <Header.Link href="#">Pull Requests</Header.Link>
+  </Header.Item>
+  <Header.Item>
+    <Header.Link href="#">Issues</Header.Link>
+  </Header.Item>
+  <Header.Item>
+    <Header.Link href="#">Codespaces</Header.Link>
+  </Header.Item>
+  <Header.Item>
+    <Header.Link href="#">Marketplace</Header.Link>
+  </Header.Item>
+  <Header.Item full>
+    <Header.Link href="#">Explore</Header.Link>
+  </Header.Item>
   <Header.Item mr={0}>
+    <BellIcon size={16} />
+  </Header.Item>
+  <Header.Item mr={0}>
+    <PlusIcon size={16} />
+    <TriangleDownIcon size={16} />
+  </Header.Item>
+  <Header.Item mr={0}>
+    <Avatar src="https://github.com/octocat.png" size={20} />
   </Header.Item>
 </Header>
   );
